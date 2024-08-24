ce5cd3dba382ed0947e45d98b8975b1d46e23aa9
tariqbio <tariqul15-13142@diu.edu.bd>
2024-08-24 18:52:14 +0600

    vugichugi

--- src/data/Home/education.js
+++ src/data/Home/education.js
@@ -9,6 +9,6 @@ export const education = [
         cgpa: "3.50/4.00",
         merit: "",
         description: "During my undergraduate studies at DIU, I developed a strong foundation in computer science principles and a keen interest in machine learning and deep learning. I actively participated in various projects and research initiatives, including a thesis where I developed a model with image classification. Overall, my time at DIU was marked by academic development, practical experience, and a passion for continuous learning and innovation in the field of computer science.",
-        images: []
+        images: ["./assets/Education/convo1.JPG","./assets/Education/convo2.JPG","./assets/Education/convo3.JPG","./assets/Education/port.jpg","./assets/certificates/BSC Certificate.png"]
     }
 ];
